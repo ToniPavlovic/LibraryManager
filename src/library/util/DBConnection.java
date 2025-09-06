8d4e052461533282b440e01ca90f83c17f602c0b
tonipvlc <tonipavlovic518@gmail.com>
2025-09-06 21:25:04 +0200

    chore: added MySQL connection and necessary drivers, so i can be prepared after i learn the sql syntax and start configuring files and merging from json to mysql

---
+++ src/library/util/DBConnection.java
@@ -0,0 +1,15 @@
+package library.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/library";
+    private static final String USER = "tester";
+    private static final String PASSWORD = "Tester123@";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
